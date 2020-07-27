d27eeb9e825e9fad44165cab7f4845df8bbd481c
unknown <nkhs@yandex.com>
2020-07-28 02:13:59 +0800

    ipdated

---
+++ doctor.js
@@ -0,0 +1,28 @@
+/*
+Put content of angular2 build into 'public' folder.
+*/
+const html = __dirname + '/public/doctor/';
+
+const port = 4000;
+const apiUrl = '/api';
+
+// Express
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const express = require('express');
+var app = express();
+
+app
+    .use(compression())
+    .use(bodyParser.json())
+    // Static content
+    .use(express.static(html))
+    // Default route
+    .use(function(req, res) {
+      res.sendFile(html + 'index.html');
+    })
+    // Start server
+    .listen(port, function () {
+        console.log('Port: ' + port);
+        console.log('Html: ' + html);
+    });
